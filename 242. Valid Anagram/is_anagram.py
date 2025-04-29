a6b2559cc7a1674f7b8209608aa0405f4fc11466
ElfatihZiad <ziad.elf@gmail.com>
2025-04-28 23:35:25 -0500

    add another solution for LC 242

--- 242. Valid Anagram/is_anagram.py
+++ 242. Valid Anagram/is_anagram.py
@@ -14,3 +14,17 @@ class Solution:
                 return False
 
         return True
+    
+    ### Another opproach using two HashMaps
+
+    def isAnagram2(self, s: str, t: str) -> bool:
+            if len(s) != len(t):
+                return False
+
+            countS, countT = {}, {}
+
+            for i in range(len(s)):
+                countS[s[i]] = 1 + countS.get(s[i], 0)
+                countT[t[i]] = 1 + countT.get(t[i], 0)
+            return countS == countT
+    
